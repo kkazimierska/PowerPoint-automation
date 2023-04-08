7a8cbf240b2a3bb2e5544eef4529bedece769e2c
Joel Tovar <96855747+Captivea-JTO@users.noreply.github.com>
2023-04-08 13:54:55 -0400

    adding shapes and images.
    - Images folder added
    - Set date and time in the name of the file

--- odoo_upgrade_presentation.py
+++ odoo_upgrade_presentation.py
@@ -2,6 +2,10 @@ import collections
 import collections.abc
 from pptx import Presentation
 from pptx.util import Inches
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.dml.color import RGBColor
+import os
+from datetime import datetime
 
 # Create a new presentation
 presentation = Presentation()
@@ -10,6 +14,19 @@ presentation = Presentation()
 def add_image(slide, image_path, left, top, width, height):
     pic = slide.shapes.add_picture(image_path, left, top, width=width, height=height)
     return pic
+
+# Add a rectangle shape with text
+def add_rectangle_with_text(slide, left, top, width, height, text, fill_color=None, text_color=None):
+    shape = slide.shapes.add_shape(MSO_SHAPE.RECTANGLE, left, top, width, height)
+    if fill_color:
+        shape.fill.solid()
+        shape.fill.fore_color.rgb = fill_color
+    if text_color:
+        shape.text_frame.text = text
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                run.font.color.rgb = text_color
+    return shape
 
 # Title slide
 title_slide_layout = presentation.slide_layouts[0]
@@ -44,11 +61,30 @@ slide2.placeholders[1].text = (
 slide3_layout = presentation.slide_layouts[1]
 slide3 = presentation.slides.add_slide(slide3_layout)
 slide3.shapes.title.text = "Python-pptx Overview"
-slide3.placeholders[1].text = (
-    "- Installing the library\n"
-    "- Basic syntax and usage\n"
-    "- Creating a PowerPoint presentation with Python"
-)
+add_rectangle_with_text(slide3, 
+                        Inches(3), 
+                        Inches(2),
+                        Inches(4),
+                        Inches(0.7), 
+                        "- Installing the library\n", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide3, 
+                        Inches(3), 
+                        Inches(3), 
+                        Inches(4), 
+                        Inches(0.7), 
+                        "- Basic syntax and usage\n", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide3, 
+                        Inches(3), 
+                        Inches(4), 
+                        Inches(4), 
+                        Inches(0.8), 
+                        "- Creating a PowerPoint presentation with Python",
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
 
 # Slide 4: Odoo Database Upgrade Process Overview
 slide4_layout = presentation.slide_layouts[1]
@@ -61,31 +97,103 @@ slide4.placeholders[1].text = (
     "- Migrating the database to the production environment"
 )
 
-# Slide 5: Exporting the Odoo Database
+# Slide 5: Exporting the Odoo Database (with shapes)
 slide5_layout = presentation.slide_layouts[1]
 slide5 = presentation.slides.add_slide(slide5_layout)
 slide5.shapes.title.text = "Exporting the Odoo Database"
-slide5.placeholders[1].text = (
-    "1. Navigate to the Odoo Database Manager\n"
-    "2. Select the database to upgrade\n"
-    "3. Click on the 'Backup' button\n"
-    "4. Choose 'zip' as the backup format\n"
-    "5. Download the backup file"
-)
-add_image(slide5, "images/export_database_diagram.png", Inches(1), Inches(3), width=Inches(4), height=Inches(2))
+
+add_rectangle_with_text(slide5, 
+                        Inches(1), 
+                        Inches(2),
+                        Inches(4),
+                        Inches(0.5), 
+                        "1. Navigate to the Odoo Database Manager", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide5, 
+                        Inches(1), 
+                        Inches(2.7), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "2. Select the database to upgrade", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide5, 
+                        Inches(1), 
+                        Inches(3.4), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "3. Click on the 'Backup' button", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide5, 
+                        Inches(1), 
+                        Inches(4.1), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "4. Choose 'zip' as the backup format", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide5, 
+                        Inches(1), 
+                        Inches(4.8), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "5. Download the backup file", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_image(slide5, "images/export_database_diagram.png", Inches(5), Inches(3), width=Inches(4), height=Inches(2))
+
+
 
 # Slide 6: Uploading the Database to the Odoo Upgrade Platform
 slide6_layout = presentation.slide_layouts[1]
 slide6 = presentation.slides.add_slide(slide6_layout)
 slide6.shapes.title.text = "Uploading the Database to the Odoo Upgrade Platform"
-slide6.placeholders[1].text = (
-    "1. Visit the Odoo Upgrade Platform (https://upgrade.odoo.com)\n"
-    "2. Log in with your Odoo account\n"
-    "3. Choose the target Odoo version\n"
-    "4. Upload the previously exported database backup file\n"
-    "5. Wait for the upgrade process to complete"
-)
-add_image(slide6, "images/upload_database_diagram.png", Inches(1), Inches(3), width=Inches(4), height=Inches(2))
+add_rectangle_with_text(slide6, 
+                        Inches(1), 
+                        Inches(2),
+                        Inches(4),
+                        Inches(0.5), 
+                        "1. Visit the Odoo Upgrade Platform (https://upgrade.odoo.com)\n", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+
+add_rectangle_with_text(slide6, 
+                        Inches(1), 
+                        Inches(2.7), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "2. Log in with your Odoo account\n", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide6, 
+                        Inches(1), 
+                        Inches(3.4), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "3. Choose the target Odoo version\n", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide6, 
+                        Inches(1), 
+                        Inches(4.1), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "4. Upload the previously exported database backup file\n", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+add_rectangle_with_text(slide6, 
+                        Inches(1), 
+                        Inches(4.8), 
+                        Inches(4), 
+                        Inches(0.5), 
+                        "5. Wait for the upgrade process to complete", 
+                        fill_color=RGBColor(221, 235, 247), 
+                        text_color=RGBColor(0, 0, 0))
+
+
+add_image(slide6, "images/upload_database_diagram.png", Inches(5), Inches(3), width=Inches(4), height=Inches(2))
 
 # Slide 7: Testing the Upgraded Database
 slide7_layout = presentation.slide_layouts[1]
@@ -115,5 +223,9 @@ slide8.placeholders[1].text = (
 add_image(slide8, "images/migration_diagram.png", Inches(1), Inches(3), width=Inches(4), height=Inches(2))
 
 # Save the presentation
-presentation.save("Odoo_Database_Upgrade_Process.pptx")
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+presentation_filename = f"Odoo_Database_Upgrade_Process_{current_datetime}.pptx"
+
+# Save the new presentation
+presentation.save(presentation_filename)
 
