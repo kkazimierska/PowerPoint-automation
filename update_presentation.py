77605e4a8c11c6716e4382b6bf401cfd59cdf8eb
KAMKA DevOps <kamka@orsted.com>
2023-11-02 14:49:02 +0100

    sample_data

--- update_presentation.py
+++ update_presentation.py
@@ -1,3 +1,31 @@
 from pptx import Presentation
+from datetime import datetime
+import pandas as pd
+
+team_competency = pd.read_csv('team_competencies.csv', sep = ";")
+
+# Documentacja: https://python-pptx.readthedocs.io/_/downloads/en/stable/pdf/
+
+# Open pptx and explore
 prs = Presentation('Draftv0.pptx')
-prs.save("Draft_readed.pptx")
+
+slide5 = prs.slides[5]
+
+# # Explore text in all shapes
+i = 0
+for shape in slide5.shapes:
+    print(i)
+    if not shape.has_text_frame:
+        continue
+    print(shape.text)
+    i +=1
+
+slide5.shapes[4].text = str(team_competency['Name'][0])
+slide5.shapes[5].text = str(team_competency['ART.'][0])
+slide5.shapes[6].text = str(team_competency['Title'][0])
+slide5.shapes[7].text = str(team_competency['Competencies'][0])
+
+
+ts = datetime.now()
+
+prs.save(f"Draft_readed{ts.microsecond}.pptx")
